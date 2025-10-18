cfbfaf7086073bf37e5ec52c7aa953db6d79f87d
spagestic <steelmax666@gmail.com>
2025-10-18 09:42:07 +0800

    fix: update grid layout for Job Positions to support extra-large screens

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -203,7 +203,7 @@ export default function DashboardPage() {
       </div>
 
       {/* Job Positions Grid */}
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3">
         {filteredJobs.map((job) => (
           <JobCard
             key={job._id}
