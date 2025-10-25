c00eccbfce02e74d3d559276c55f18f03a6fb8e3
spagestic <steelmax666@gmail.com>
2025-10-25 21:10:37 +0800

    fix: update button text for interview link in JobCard component

--- components/jobs/job-card.tsx
+++ components/jobs/job-card.tsx
@@ -221,7 +221,7 @@ export const JobCard: React.FC<JobCardProps> = ({ job }) => {
         </div>
 
         <Button asChild className="w-full" size="sm">
-          <Link href={`/call/${job.id}`}>Start Practice Interview</Link>
+          <Link href={`/call/${job.id}`}>Start Interview</Link>
         </Button>
       </CardContent>
     </Card>
