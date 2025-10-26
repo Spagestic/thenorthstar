50351f29b4b1a16e792c93f694b6917a8bc4d892
spagestic <steelmax666@gmail.com>
2025-10-26 18:07:22 +0800

    feat: implement user redirection to dashboard on login and sign-up pages

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,16 @@
 import { LoginForm } from "@/components/auth/login-form";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const supabase = await createClient();
+  const { data } = await supabase.auth.getClaims();
+  const user = data?.claims;
+
+  if (user) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
