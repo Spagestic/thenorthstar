e8b63fbff97a2b997e44b6d9652c9c75f6d3e549
spagestic <steelmax666@gmail.com>
2025-11-09 13:41:16 +0800

    refactor: restructure Page component to use Suspense for ErrorContent loading

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -1,6 +1,25 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Suspense } from "react";
 
-export default async function Page({
+export default function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ error: string }>;
+}) {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <div className="flex flex-col gap-6">
+          <Suspense fallback={<div>Loading...</div>}>
+            <ErrorContent searchParams={searchParams} />
+          </Suspense>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+async function ErrorContent({
   searchParams,
 }: {
   searchParams: Promise<{ error: string }>;
@@ -8,29 +27,21 @@ export default async function Page({
   const params = await searchParams;
 
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <div className="flex flex-col gap-6">
-          <Card>
-            <CardHeader>
-              <CardTitle className="text-2xl">
-                Sorry, something went wrong.
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              {params?.error ? (
-                <p className="text-sm text-muted-foreground">
-                  Code error: {params.error}
-                </p>
-              ) : (
-                <p className="text-sm text-muted-foreground">
-                  An unspecified error occurred.
-                </p>
-              )}
-            </CardContent>
-          </Card>
-        </div>
-      </div>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-2xl">Sorry, something went wrong.</CardTitle>
+      </CardHeader>
+      <CardContent>
+        {params?.error ? (
+          <p className="text-sm text-muted-foreground">
+            Code error: {params.error}
+          </p>
+        ) : (
+          <p className="text-sm text-muted-foreground">
+            An unspecified error occurred.
+          </p>
+        )}
+      </CardContent>
+    </Card>
   );
 }
