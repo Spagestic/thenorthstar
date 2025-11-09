4aa4481d4a393b3d1e8693502fdcb62c2e901ec1
spagestic <steelmax666@gmail.com>
2025-11-09 22:04:26 +0800

    fix: remove StatsSection import and usage from Home component

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -2,13 +2,11 @@ import FAQ from "@/components/faq-section";
 import { Features } from "@/components/features";
 import PricingSection from "@/components/pricing-section";
 import Hero2 from "@/components/Hero2";
-import StatsSection from "@/components/stats-section";
 
 export default function Home() {
   return (
     <div>
       <Hero2 />
-      <StatsSection />
       <Features />
       <PricingSection />
       <FAQ />
