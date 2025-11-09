8d2c052bd8a43a004a162030c6f0b9d408e47f24
spagestic <steelmax666@gmail.com>
2025-11-09 13:38:07 +0800

    refactor: wrap ProfileHeader and ProfileContent in Suspense for improved loading handling

--- app/(protected)/profile/page.tsx
+++ app/(protected)/profile/page.tsx
@@ -1,13 +1,19 @@
 import ProfileHeader from "./components/profile-header";
 import ProfileContent from "./components/profile-content";
 import Header from "../Header";
+import { Suspense } from "react";
+
 export default function Page() {
   return (
     <div>
       <Header nav={["Profile"]} />
       <div className="container mx-auto space-y-6 px-4">
-        <ProfileHeader />
-        <ProfileContent />
+        <Suspense>
+          <ProfileHeader />
+        </Suspense>
+        <Suspense>
+          <ProfileContent />
+        </Suspense>
       </div>
     </div>
   );
