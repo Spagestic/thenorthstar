4ddf5cfac404928ff4bee356c73735fc25d5d02b
spagestic <steelmax666@gmail.com>
2025-11-09 14:13:02 +0800

    refactor: update logout function to redirect to home and refresh the router

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -32,7 +32,8 @@ export function UserAvatar({
   const logout = async () => {
     const supabase = createClient();
     await supabase.auth.signOut();
-    router.push("/auth/login");
+    router.push("/");
+    router.refresh();
   };
 
   return (
