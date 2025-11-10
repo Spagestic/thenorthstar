0f14de7e7e495b20587fd952c23bf4e86c9c1a1d
spagestic <steelmax666@gmail.com>
2025-11-10 09:28:59 +0800

    fix: adjust padding in Footer component for improved layout

--- components/footer-section.tsx
+++ components/footer-section.tsx
@@ -32,7 +32,7 @@ export function Footer({
   copyright,
 }: FooterProps) {
   return (
-    <footer className="pt-16 pb-6 lg:pt-24 lg:pb-8">
+    <footer className="pt-8 pb-6 lg:pt-16 lg:pb-8">
       <div className="px-4 lg:px-8">
         <div className="md:flex md:items-start md:justify-between">
           <Link
